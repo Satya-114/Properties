c221fd3ec824e8f9538e5886b8f9c82c1646a5f4
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-19 17:40:48 +0530

    Add files via upload

---
+++ Read & write only properties/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Read_and_write_only_properties
+{
+   public class example
+    {
+        int num1, num2, result;
+        public int setnum1
+        {
+            set
+            {
+                num1 = value;
+            }
+        }
+        public int setnum2
+        {
+            set
+            {
+                num2 = value;
+            }
+        }
+        public int getresult
+        {
+            get
+            {
+                return result;
+            }
+        }
+        public void add()
+        {
+            result = num1 + num2;
+        }
+        public void sub()
+        {
+            result = num1 - num2;
+        }
+        public void mul()
+        {
+            result = num1 * num2;
+        }
+        public void div()
+        {
+            result = num1 / num2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            example obj1 = new example();
+            Console.WriteLine("ENTER ANY TWO NUMBERS");
+            obj1.setnum1 = int.Parse(Console.ReadLine());
+            obj1.setnum2 = int.Parse(Console.ReadLine());
+            obj1.add();
+            Console.WriteLine("the sum is:" + obj1.getresult);
+            obj1.sub();
+            Console.WriteLine("the sub is:" + obj1.getresult);
+            obj1.mul();
+            Console.WriteLine("the mul is:" + obj1.getresult);
+            obj1.div();
+            Console.WriteLine("the div is:" + obj1.getresult);
+            Console.ReadKey();
+        }
+    }
+}   
+   
+
